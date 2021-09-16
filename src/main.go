c1d37cade0257b2649f8cf4d6f0907a562910c05
FFO96 <ffoliveres@gmail.com>
2021-09-17 00:55:24 +0200

    Added recursive method with pointers implementation

--- src/main.go
+++ src/main.go
@@ -4,10 +4,14 @@ import "fmt"
 
 func main() {
 
-	orderedArray := []int{1, 2, 4, 5, 8, 9, 10}
+	orderedArray := []int{1, 2, 4, 5, 8, 9, 10, 12, 13, 14, 15, 16, 17, 60, 200, 400, 500}
 	target := 4
+	first := 0
+	last := len(orderedArray) - 1
+
 	fmt.Println(iterChop(target, orderedArray))
-	fmt.Println(recursiveChop(target, 0, len(orderedArray)-1, orderedArray))
+	fmt.Println(recursiveChop(target, first, last, orderedArray))
+	fmt.Println(pRecursiveChop(&target, &first, &last, &orderedArray))
 
 }
 
@@ -44,6 +48,26 @@ func recursiveChop(target, first, last int, orderedArray []int) int {
 		} else if orderedArray[mid] < target {
 			first = mid + 1
 			return recursiveChop(target, mid+1, last, orderedArray)
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
+
+// Third implementation: Recursive method using pointers
+func pRecursiveChop(target, first, last *int, orderedArray *[]int) int {
+
+	if *first <= *last {
+
+		mid := (*first + *last) / 2
+
+		if (*orderedArray)[mid] > *target {
+			*last = mid - 1
+			return pRecursiveChop(target, first, last, orderedArray)
+		} else if (*orderedArray)[mid] < *target {
+			*first = mid + 1
+			return pRecursiveChop(target, first, last, orderedArray)
 		} else {
 			return mid
 		}
