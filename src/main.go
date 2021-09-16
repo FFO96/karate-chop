f20906562c8d22497d3bc32b52e8d730e0c8ed27
FFO96 <ffoliveres@gmail.com>
2021-09-17 01:50:06 +0200

    Comments added

--- src/main.go
+++ src/main.go
@@ -9,10 +9,9 @@ func main() {
 	first := 0
 	last := len(orderedArray) - 1
 
-	fmt.Println(iterChop(target, orderedArray))
-	fmt.Println(recursiveChop(target, first, last, orderedArray))
-	fmt.Println(pRecursiveChop(&target, &first, &last, &orderedArray))
-
+	fmt.Printf("Iterative chop result: %v\n", iterChop(target, orderedArray))
+	fmt.Printf("Recursive chop result: %v\n", recursiveChop(target, first, last, orderedArray))
+	fmt.Printf("Recursive chop with pointers result: %v\n", pRecursiveChop(&target, &first, &last, &orderedArray))
 }
 
 // First implementation: Iteration method
@@ -21,18 +20,25 @@ func iterChop(target int, orderedArray []int) int {
 	first := 0
 	last := len(orderedArray) - 1
 
+	// While the inferior index is not bigger than the superior index, it will keep searching
 	for first <= last {
 
+		// Calculating array middle point
 		mid := (first + last) / 2
 
+		// Target less than number in middle point
 		if orderedArray[mid] > target {
 			last = mid - 1
+
+			// Target greater than number in middle point
 		} else if orderedArray[mid] < target {
 			first = mid + 1
 		} else {
+			// Target found
 			return mid
 		}
 	}
+	// Target not in array
 	return -1
 }
 
@@ -41,36 +47,48 @@ func recursiveChop(target, first, last int, orderedArray []int) int {
 
 	if first <= last {
 
+		// While the inferior index is not bigger than the superior index, it will keep searching
 		mid := (first + last) / 2
 
+		// Target less than number in middle point
 		if orderedArray[mid] > target {
 			return recursiveChop(target, first, mid-1, orderedArray)
+
+			// Target greater than number in middle point
 		} else if orderedArray[mid] < target {
 			first = mid + 1
 			return recursiveChop(target, mid+1, last, orderedArray)
 		} else {
+			// Target found
 			return mid
 		}
 	}
+	// Target not in array
 	return -1
 }
 
 // Third implementation: Recursive method using pointers
 func pRecursiveChop(target, first, last *int, orderedArray *[]int) int {
 
+	// While the inferior index is not bigger than the superior index, it will keep searching
 	if *first <= *last {
 
 		mid := (*first + *last) / 2
 
+		// Target less than number in middle point
 		if (*orderedArray)[mid] > *target {
 			*last = mid - 1
 			return pRecursiveChop(target, first, last, orderedArray)
+
+			// Target greater than number in middle point
 		} else if (*orderedArray)[mid] < *target {
 			*first = mid + 1
 			return pRecursiveChop(target, first, last, orderedArray)
 		} else {
+			// Target found
 			return mid
 		}
 	}
+	// Target not in array
 	return -1
 }
